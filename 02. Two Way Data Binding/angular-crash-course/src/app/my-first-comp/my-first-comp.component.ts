c25550cc92c3b221910a6e4d66d26266e3f6bda8
abhisheksat <abhisheksatpute11@gmail.com>
2024-10-01 14:20:16 -0400

    Two Way Data Binding | Form fields with two way data binding [(ngModel)] and {{fieldName}}

---
+++ 02. Two Way Data Binding/angular-crash-course/src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  templateUrl: './my-first-comp.component.html',
+  styleUrls: ['./my-first-comp.component.scss']
+})
+export class MyFirstCompComponent {
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+}
